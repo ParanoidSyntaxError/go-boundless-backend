c9bc661cedd670b0e69c841e2d567bfde7fcaba7
Andrew Pohl <andypohl@hotmail.co.uk>
2024-11-06 13:00:33 +0000

    update model for user info

--- API/Auth/models.py
+++ API/Auth/models.py
@@ -5,7 +5,7 @@ class UserModel(db.Model):
     __tablename__ = "users" 
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    dent_uid = db.Column(db.Integer, unique=True, nullable=True)
+    dent_uid = db.Column(db.String(80), unique=True, nullable=True)
     email = db.Column(db.String(255), unique=True, nullable=False)
     birthdate = db.Column(db.Date, nullable=True)
     currency = db.Column(db.String(10), nullable=False, default="USD")
@@ -18,7 +18,7 @@ class UserModel(db.Model):
     last_name = db.Column(db.String(255), nullable=False)
     password = db.Column(db.String(255), nullable=False)
     is_email_verified = db.Column(db.Boolean, default=False)
-    verification_code = db.Column(db.String(4), nullable=True)
+    verification_code = db.Column(db.String(6), nullable=True)
     verification_code_expiry = db.Column(db.DateTime, nullable=True)
     reset_password_token = db.Column(db.String(36), nullable=True)
     date_created = db.Column(db.DateTime, nullable=False, default=lambda: datetime.now(timezone.utc))
